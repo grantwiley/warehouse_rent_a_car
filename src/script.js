9867e847e8ecd839287f001df2d7ef189354e4e6
Grant Wiley <grantlwiley@gmail.com>
2025-03-04 12:29:33 -0500

    Update EmailJS service and template names for rental quote form

--- src/script.js
+++ src/script.js
@@ -908,8 +908,8 @@ class RentalQuoteForm {
     };
 
     Promise.all([
-      emailjs.send('service_b8f7bys', 'customer_quote', emailData),
-      emailjs.send('service_b8f7bys', 'internal_quote', emailData),
+      emailjs.send('caw_rental', 'customer_quote', emailData),
+      emailjs.send('caw_rental', 'internal_quote', emailData),
     ])
       .then(() => this.showThankYouMessage())
       .catch((error) => {
